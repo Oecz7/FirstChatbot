a0d4bb8916fadb5d6875a891c2bd3d6fa1815b53
Oecz7 <33704858+Oecz7@users.noreply.github.com>
2017-11-17 16:51:10 -0500

    messageText

--- app.js
+++ app.js
@@ -315,7 +315,7 @@ function handleMessage(currentUser, senderID, message, isEcho, messageId, appId,
     }
     else {
       //sendTextMessage(senderID, messageText);
-		sendToBot(senderID,message);
+		sendToBot(senderID,messageText);
     }
   }
   else if (messageAttachments) {
